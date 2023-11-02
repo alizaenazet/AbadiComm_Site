41518a96c4f8c46698bd880bcbe168de64f29b62
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-02 14:26:57 +0700

    feat:add tailwind configuration for fontsize

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,25 @@ export default {
       'sans': ['Poppins','sans-serif'],
       'serif': ['Gabarito','serif']
     },
+    fontSize: {
+      'headline-lg': ['72px'],
+      'h1-lg': ['48px'],
+      'h2-lg': ['32px'],
+      'h3-lg': ['24px'],
+      'medium-lg': ['18px'],
+      'normal-lg': ['16px'],
+      'input-lg': ['15px'],
+
+      'headline-sm': ['30px'],
+      'h1-sm': ['21px'],
+      'h2-sm': ['18.5px'],
+      'h3-sm': ['17.5px'],
+      'medium-sm': ['16px'],
+      'normal-sm': ['14px'],
+      'input-sm': ['13px']
+    },
+    padding:{
+    },
     extend: {},
   },
   plugins: [
