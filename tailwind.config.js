8dd8bc5edf4409d0906a61bbef727dd8725b9a3b
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-02 13:46:21 +0700

    feat:add configurasion for colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    colors:{
+      'primary': '#001C55',
+      'secondary': '#DD1C1A',
+      'accent': '#FFCF56',
+      'error': '#F9B3D1',
+      'success': '#7BD389',
+    },
+    fontFamily: {
+      'sans': ['Poppins','sans-serif'],
+      'serif': ['Gabarito','serif']
+    },
     extend: {},
   },
   plugins: [
