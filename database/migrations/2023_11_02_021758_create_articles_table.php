ea7840722505496f6bbe8b35d7e380ca09b991a7
jerrytjahyono <120613631+jerrytjahyono@users.noreply.github.com>
2023-11-02 11:14:14 +0700

    feat: add all migrations

--- database/migrations/2023_11_02_021758_create_articles_table.php
+++ database/migrations/2023_11_02_021758_create_articles_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title',200);
+            $table->string('image_url',400);
+            $table->date('date');
+            $table->string('content',5000);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
