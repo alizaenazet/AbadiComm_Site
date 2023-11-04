3e1eae4d017f544db10bd6683c6249d369256a46
jerrytjahyono <120613631+jerrytjahyono@users.noreply.github.com>
2023-11-04 17:28:35 +0700

    feat: add view component blade route list-portfolio

---
+++ app/View/Components/PortfolioCard.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class PortfolioCard extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $image,
+        public string $title,
+        public string $date,
+        public string $promoter,
+        
+        )
+    {
+        
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        // return view('components.portfolio-card');
+        return "<div>COK </div>";
+    }
+}
