32ea5b1d07fba43004742542b4709415a25822b5
jerrytjahyono <120613631+jerrytjahyono@users.noreply.github.com>
2023-11-04 21:36:56 +0700

    refactor: change component team member

--- resources/views/components/member-card.blade.php
+++ resources/views/components/member-card.blade.php
@@ -1,18 +1,18 @@
-<div class="flex flex-col justify-center  gap-10 bg-accent rounded-xl px-[13px] pt-[27px] pb-[58px] "> 
-    <div class="flex justify-center w-[280px] h-[213px] bg-red-800">
+<div class="w-[301px] h-ull flex-warp flex flex-col gap-3 bg-gradient-to-br from-primary from-45% to-secondary rounded-xl px-[20px] py-[27px]"> 
+
+    <div class="flex justify-center backdrop-blur-md rounded-lg" style="box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37)">
         <img class="object-cover rounded-md max-h-[213px] " src={{ $imageUrl }} alt="">
     </div>
 
-    <div class="flex-col items-start gap-20">
+    <div class="flex-col items-start flex ">
         <div class="">
-            <h1 class="text-medium-lg font-sans text-white" style="gap: 10px"> {{ $name }} </h1>
+            <h1 class="font-bold text-h3-lg font-sans text-white"> {{ $name }} </h1>
         </div>
         <div>
-            <h4 class="text-white text-normal-lg font-sans">{{ $divisionName }}</h4>
+            <h4 class="font-bold text-white text-normal-lg font-sans">{{ $divisionName }}</h4>
         </div>
         <div>
             <p class="text-white text-input-lg font-sans">{{ $qualification }}</p>
         </div>
     </div>
-
 </div>
--- resources/views/components/pages/team-member.blade.php
+++ resources/views/components/pages/team-member.blade.php
@@ -2,10 +2,9 @@
     gap="30px" title="TeamMember">
 
     <h1 class="text-h1-lg font-serif">Be Our Partner</h1>
-    <div class="flex flex-row w-full justify-center flex-wrap" style="gap: 20px">
+    <div class="flex flex-row w-full h-fit justify-around flex-wrap" style="gap: 20px">
         @foreach ($members as $member)
         <x-member-card 
-           
             name='{{$member->name}}'
             divisionName='{{$member->division->name}}'
             imageUrl='{{$member->image_url}}'
