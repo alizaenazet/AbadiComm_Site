a0b5e4145d137f62ff56505fc82d78677a83dcf8
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-07 15:33:53 +0700

    remove and add contact-welcome component

--- resources/views/components/pages/about-us.blade.php
+++ resources/views/components/pages/about-us.blade.php
@@ -43,7 +43,6 @@
         </div>
 
     </div>
-</div>
 
 <div>
     <h1 class="w-full text-h1-lg font-serif font-bold text-center">Our clients</h1>
@@ -99,26 +98,4 @@
             See details</h3>
     </a>
 </div>
-
-
-</div>
-
-<div>
-    <div class="flex flex-row gap-[23px]">
-        <div class="flex-1 ">
-            <img class="object-cover h-full" src="https://random.imagecdn.app/300/300" alt="">
-        </div>
-        <div class="flex-1 flex flex-col gap-6">
-            <h1 class="text-h1-lg font-serif">Be Our Partner</h1>
-            <p> repellat adipisci eligendi. Dolores dolor est doloribus iure ipsum dolore totam. Numquam
-                occaecati quia qui. Neque cum eum earum incidunt a provident rerum. Assumenda nobis beatae
-            </p>
-            <button data-modal-target="contact-modal" data-modal-toggle="contact-modal" class="px-[20px] py-[10px] w-[181px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button">
-                Take a step
-              </button>
-            <button data-modal-target="another-modal" data-modal-toggle="another-modal" class="px-[20px] py-[10px] w-[181px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button">
-                tes
-              </button>
-        </div>
-    </div>
 </div>
---
+++ resources/views/components/pages/welcome-contact.blade.php
@@ -0,0 +1,15 @@
+<div class="flex flex-row gap-[23px]">
+    <div class="flex-1 ">
+        <img class="object-cover h-full" src="https://random.imagecdn.app/300/300" alt="">
+    </div>
+    <div class="flex-1 flex flex-col gap-6">
+        <h1 class="text-h1-lg font-serif">Be Our Partner</h1>
+        <p> repellat adipisci eligendi. Dolores dolor est doloribus iure ipsum dolore totam. Numquam
+            occaecati quia qui. Neque cum eum earum incidunt a provident rerum. Assumenda nobis beatae
+        </p>
+        <button data-modal-target="contact-modal" data-modal-toggle="contact-modal" class="px-[20px] py-[10px] w-[181px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button">
+            Take a step
+          </button>
+        
+    </div>
+</div>
