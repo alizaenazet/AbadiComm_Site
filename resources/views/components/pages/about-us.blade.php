2eee6f3642c29455a46631da297f893b3f6ab758
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-07 15:26:38 +0700

    remove and add gallery component

--- resources/views/components/pages/about-us.blade.php
+++ resources/views/components/pages/about-us.blade.php
@@ -103,33 +103,6 @@
 
 </div>
 
-
-<div>
-    <h1 class="w-full text-h1-lg font-serif font-bold text-center">Activity Gallery</h1>
-    <div class="py-[31px] w-screen flex flex-center justify-center">
-        <div class="w-[80%] flex flex-center justify-center flex-row flex-wrap gap-x-8 gap-y-3 ">
-            <img src="https://random.imagecdn.app/300/300" alt="" class= "w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md ">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-            <img src="https://random.imagecdn.app/300/300" alt="" class="w-32 h-32 rounded-md">
-        </div>
-    </div>
-    <div class="flex flex-warp justify-center items-center">
-        <form action="">
-            <button type="button" style="padding: 2px 6px"
-                class="border-2 flex flex-row items-center gap-1 w-fit  px-8 border-blue-500 font-medium rounded-lg text-center ">
-                Show More
-            </button>
-        </form>
-    </div>
-</div>
-
 <div>
     <div class="flex flex-row gap-[23px]">
         <div class="flex-1 ">
