43092906a82e33bcd2008619c76e109b04413e50
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-07 13:50:19 +0700

    Update partner.blade.php

--- resources/views/components/pages/partner.blade.php
+++ resources/views/components/pages/partner.blade.php
@@ -18,11 +18,9 @@
                 repellat adipisci eligendi. Dolores dolor est doloribus iure ipsum dolore totam. Numquam 
                 occaecati quia qui. Neque cum eum earum incidunt a provident rerum. Assumenda nobis beatae 
             </p>
-            {{-- <button type="button" class="px-[20px] py-[10px] w-[181px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                Take a step
-            </button> --}}
+
             <button data-modal-target="partner-modal" data-modal-toggle="partner-modal" class="px-[20px] py-[10px] w-[181px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button">
-                Contact us
+                Take a step
               </button>
             
               <!-- Main modal -->
