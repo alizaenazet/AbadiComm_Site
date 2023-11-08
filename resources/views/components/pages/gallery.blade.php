2e3b4ce4b3b4a8afc93191115b8cf42495581feb
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-08 19:43:33 +0700

    feat: add responsive css

--- resources/views/components/pages/gallery.blade.php
+++ resources/views/components/pages/gallery.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout 
     gap="20px" title="Gallery" >
     
-    <h1 class="text-h1-lg font-serif">Activity gallery</h1>
-    <div id="galleryList" class="w-full h-fit flex flex-row flex-wrap gap-3 justify-center">
+    <h1 class="text-h1-lg text-center font-serif">Activity gallery</h1>
+    <div id="galleryList" class="w-full h-fit flex flex-row flex-wrap gap-2 md:gap-3 justify-center">
         @foreach ($galleries as $gallery)
-        <img id={{$gallery->id}} class=" imageList w-[164px] h-[164px] sm:w-[100px] sm:h-[100px] rounded-md hover:-translate-y-1 hover:scale-500" src={{ $gallery->image_url }} alt={{ $gallery->content }}>
+        <img id={{$gallery->id}} class=" imageList w-[64px] md:w-[164px] h-[64px] md:h-[164px] sm:w-[100px] sm:h-[100px] rounded-md hover:-translate-y-1 hover:scale-500" src={{ $gallery->image_url }} alt={{ $gallery->content }}>
         @endforeach
     </div>
 
@@ -24,7 +24,7 @@
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
             <div class=" flex justify-center relative transform overflow-hidden rounded-lg  text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                     <div  class="flex flex-col flex-wrap justify-center items-center p-[15px] h-fit w-fit bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100 gap-4">
                         <div>
