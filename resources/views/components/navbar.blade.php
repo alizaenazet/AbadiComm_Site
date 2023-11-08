b9d82f63a23d8a0788f00a4c0c0f217759d162da
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-11-08 10:40:25 +0700

    feat: add humberger menu for navbar; padding for responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,7 +6,7 @@
     <img src="/images/logo_small.svg" alt="">
 </a>
 
-<div class="flex flex-row gap-9">
+<div class="md:flex flex-row gap-9 hidden ">
     <a class="hover:border-b-2 hover:border-accent"
      href="/list-portfolio">
         <p>Portfolio</p>
@@ -26,8 +26,28 @@
 </div>
 
 
+<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white   focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:hidden" type="button">
+    <svg class="fill-primary" xmlns="http://www.w3.org/2000/svg" height="34" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+</button>
+
+<!-- Dropdown menu -->
+<div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 ">
+    <ul class="p-[8px] text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <a href="/list-portfolio" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Portfolio</a>
+      </li>
+      <li>
+        <a href="/gallery" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Gallery</a>
+      </li>
+      <li>
+        <a href="/partner" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Partner</a>
+      </li>
+    </ul>
+</div>
+
+
 <!-- Modal toggle -->
-<button data-modal-target="contact-modal" data-modal-toggle="contact-modal" class=" px-[10px] py-[8px] text-gray-900 w-max  bg-white border-2 border-primary focus:outline-none hover:bg-gray-100 focus:ring-4 hover:border-accent focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+<button data-modal-target="contact-modal" data-modal-toggle="contact-modal" class=" px-[10px] py-[8px] text-gray-900 w-max  bg-white border-2 border-primary focus:outline-none hover:bg-gray-100 focus:ring-4 hover:border-accent focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 hidden md:flex" type="button">
     Contact us
   </button>
 
